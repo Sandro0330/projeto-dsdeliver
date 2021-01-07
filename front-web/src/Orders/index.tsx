d7107c44cd3757b71a47a9cd05b0b206412dbfa0
Sandro0330 <sandro030486@gmail.com>
2021-01-07 16:05:05 -0300

    Add routes and StepsHeader(cabeçalho de etapas)

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import StepsHeader from './StepsHeader';
+import './style.css';
+
+function Orders() {
+    return (
+       <div className="orders-container">
+           <StepsHeader/>
+       </div>
+
+    )
+}
+
+export default Orders;
